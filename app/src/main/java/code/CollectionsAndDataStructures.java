551e4969832a6ed665166bb2371e40ec1692fc55
oozoemenam <oozoemenam@gmail.com>
2023-06-25 16:58:18 +0200

    bubble sort

--- app/src/main/java/code/CollectionsAndDataStructures.java
+++ app/src/main/java/code/CollectionsAndDataStructures.java
@@ -9,13 +9,24 @@ public class CollectionsAndDataStructures {
   
   public static void main(String[] args) {
     
-    Melon[] melons = new Melon[] {
-      new Melon("type", 3),
-      new Melon("type", 1),
-      new Melon("type", 5)
-    };
+    int[] arr = new int[] {9, 1, 6, 3, 4, 3, 5, 2, 7};
     
-    System.out.println(Arrays.toString(compareSort(melons)));
+    System.out.println(Arrays.toString(bubbleSort(arr)));
+  }
+
+  // O(n^2)
+  public static int[] bubbleSort(int[] arr) {
+    int n = arr.length;
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          int temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+    return arr;
   }
 
   public static int[] easySort(int[] in) {
