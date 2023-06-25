c051d88d15dec89c642a16ba8a5c9da24de26af9
oozoemenam <oozoemenam@gmail.com>
2023-06-25 16:50:27 +0200

    compare sort

--- app/src/main/java/code/CollectionsAndDataStructures.java
+++ app/src/main/java/code/CollectionsAndDataStructures.java
@@ -1,15 +1,35 @@
 package code;
 
 import java.util.Arrays;
+import java.util.Comparator;
+
+import code.util.Melon;
 
 public class CollectionsAndDataStructures {
   
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(easySort(new int[]{1, 3, 2, 5, 2})));
+    
+    Melon[] melons = new Melon[] {
+      new Melon("type", 3),
+      new Melon("type", 1),
+      new Melon("type", 5)
+    };
+    
+    System.out.println(Arrays.toString(compareSort(melons)));
   }
 
   public static int[] easySort(int[] in) {
     Arrays.sort(in);
     return in;
+  }
+
+  public static Melon[] compareSort(Melon[] melons) {
+    Arrays.sort(melons, new Comparator<Melon>() {
+      @Override
+      public int compare(Melon melon1, Melon melon2) {
+        return Integer.compare(melon1.getWeight(), melon2.getWeight());
+      }
+    });
+    return melons;
   }
 }
---
+++ app/src/main/java/code/util/Melon.java
@@ -0,0 +1,24 @@
+package code.util;
+
+public class Melon {
+  private final String type;
+  private final int weight;
+
+  public Melon(String type, int weight) {
+    this.type = type;
+    this.weight = weight;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public int getWeight() {
+    return weight;
+  }
+
+  @Override
+  public String toString() {
+    return "<Melon: type=" + type + ", weight=" + weight + ">";
+  }
+}
