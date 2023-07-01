5b027fbe9cdd5b6060c1983cbe77e0d2d3ddc0d9
oozoemenam <oozoemenam@gmail.com>
2023-07-01 14:27:18 +0200

    insertion sort

--- app/src/main/java/code/CollectionsAndDataStructures.java
+++ app/src/main/java/code/CollectionsAndDataStructures.java
@@ -9,13 +9,23 @@ public class CollectionsAndDataStructures {
   
   public static void main(String[] args) {
     
-    Melon[] melons = new Melon[] {
-      new Melon("type2", 3),
-      new Melon("type3", 1),
-      new Melon("type1", 5)
-    };    
-    Comparator<Melon> byType = Comparator.comparing(Melon::getType);
-    System.out.println(Arrays.toString(bubbleSortWithComparator(melons, byType)));
+    int[] arr = new int[] {9, 1, 6, 3, 4, 3, 5, 2, 7};
+    
+    System.out.println(Arrays.toString(insertionSort(arr)));
+  }
+
+  public static int[] insertionSort(int arr[]) {
+    int n = arr.length;
+    for (int i = 1; i < n; ++i) {
+      int key = arr[i];
+      int j = i - 1;
+      while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j = j - 1;
+      }
+      arr[j + 1] = key;
+    }
+    return arr;
   }
 
   public static <T> T[] bubbleSortWithComparator(T arr[], Comparator<? super T> c) {
